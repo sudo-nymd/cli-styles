8d7f06590325f6afdf8db2adfbe1ab2b7b1a2373
Tyler Durham <TylerDurham@users.noreply.github.com>
2022-06-20 00:14:27 -0500

    Default exports.

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,3 @@
-import config from './common/config';
-import * as colors from 'ansi-colors';
-import * as pkg from '../package.json';
+import cliColors from './cli-colors';
 
-const sayHello = (name: string) => {
-    return `${colors.magentaBright.bold('Hello')}, ${colors.bgBlueBright.bold(name)}!`
-}
-
-console.log("\n" + sayHello(pkg.name) + "\n");
-
-export { sayHello }
+export default cliColors;
