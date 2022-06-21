fe1a4898256a9116b8d24568fd969940b6932c2d
Tyler Durham <TylerDurham@users.noreply.github.com>
2022-06-21 11:44:31 -0500

    Fixed bgWhite incorrectly marked as foreground

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,8 @@
 import { ColorClass, StyleCode, StyleCodeType, StyleFunction } from "./cli-colors";
 import cliColors from './cli-colors';
+import { AnsiStyleCodes } from "./common/colors";
+import { StyleCodeArray } from "./common/types";
 
 export default cliColors;
 
-export { ColorClass, StyleCode, StyleCodeType, StyleFunction }
+export { ColorClass, StyleCode, StyleCodeType, StyleFunction, AnsiStyleCodes, StyleCodeArray }
