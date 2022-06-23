be50d4347dfdf81311410283dcac96768cfb3b41
Tyler Durham <TylerDurham@users.noreply.github.com>
2022-06-23 18:49:51 -0500

    reset

--- src/common/transparent.ts
+++
@@ -1,4 +0,0 @@
-import { AnsiColorCodeTypes, AnsiStyleCodeTypes } from "./types";
-
-export const TRANSPARENT_COLOR_CODE = 
-    { name: "transparent", value: [49, 7], type: AnsiStyleCodeTypes.Color, colorType: AnsiColorCodeTypes.Transparent }
