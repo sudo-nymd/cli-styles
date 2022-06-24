40cb2f4be66a0951cb15b18034164513bb34fff7
Tyler Durham <TylerDurham@users.noreply.github.com>
2022-06-24 00:05:53 -0500

    changes

--- src/effects/arrow.ts
+++ src/effects/arrow.ts
@@ -6,40 +6,40 @@ import { ansiStyles } from '../ansi-styles'
 
 //const { transparent, encode, convertToForeground } = utils;
 
-    export enum ArrowDirections {
-        //Right = "\ue0b0",
-        Right = "right",
-        //Left = "\ue0b2",
-        Left = 'left'
-    }
+export enum ArrowDirections {
+    //Right = "\ue0b0",
+    Right = "right",
+    //Left = "\ue0b2",
+    Left = 'left'
+}
 
-    export type ArrowOptions = {
-        fgColor?: AnsiColorCode;
-        bgColor?: AnsiColorCode;
-        direction?: ArrowDirections;
-    }
+export type ArrowOptions = {
+    fgColor?: AnsiColorCode;
+    bgColor?: AnsiColorCode;
+    direction?: ArrowDirections;
+}
 
-    const DEFAULT = {
-        fgColor: ansiStyles.codes.white,
-        bgColor: ansiStyles.codes.bgBlackBright,
-        direction: ArrowDirections.Right
-    }
+const DEFAULT = {
+    fgColor: ansiStyles.codes.white,
+    bgColor: ansiStyles.codes.bgBlackBright,
+    direction: ArrowDirections.Right
+}
 
-    export const arrow = (options?: ArrowOptions) => {
-        const opts = Object.assign(DEFAULT, options);
-        const { bgColor, fgColor, direction } = opts;
-        const afg = utils.convertToForeground(bgColor);
-        const symbol = effects.symbols.arrows[direction];
+export const arrow = (options?: ArrowOptions) => {
+    const opts = Object.assign(DEFAULT, options);
+    const { bgColor, fgColor, direction } = opts;
+    const afg = utils.convertToForeground(bgColor);
+    const symbol = effects.symbols.arrows[direction];
 
-        return (text: string) => {
-            if (direction === ArrowDirections.Right) {
-                return utils.transparent(symbol, afg) + utils.encode(utils.encode(text, fgColor), bgColor) + utils.encode(symbol, afg);
-            } else {
-                return utils.encode(symbol, afg) + utils.encode(utils.encode(text, fgColor), bgColor) + utils.transparent(symbol, afg);
-            }
+    return (text: string) => {
+        if (direction === ArrowDirections.Right) {
+            return utils.transparent(symbol, afg) + utils.encode(utils.encode(text, fgColor), bgColor) + utils.encode(symbol, afg);
+        } else {
+            return utils.encode(symbol, afg) + utils.encode(utils.encode(text, fgColor), bgColor) + utils.transparent(symbol, afg);
         }
     }
+}
 
-    export const ModuleName = "arrow";
+export const ModuleName = "arrow";
 
 
